ef126dd818b33c79e3f50302f9ed7dc69772a44d
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-20 05:35:58 +0300

    Complete lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    # knowledge =[]
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+    def learn(self, strng):
+      self.knowledge.append(strng)
+        
